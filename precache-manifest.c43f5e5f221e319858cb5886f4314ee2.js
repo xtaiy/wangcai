dca8998797f8e40a05c0d33d58756364d2dcdac6
taiy <1726311422@qq.com>
2022-06-08 21:37:37 +0800

    deploy

---
+++ precache-manifest.c43f5e5f221e319858cb5886f4314ee2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63695cc81c51db69598f",
+    "url": "/wangcai-website/css/app.639b68bf.css"
+  },
+  {
+    "revision": "2afdc6441990645fce21",
+    "url": "/wangcai-website/css/chunk-vendors.9181e156.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/wangcai-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/wangcai-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "71d34557873fc66ff4fbbce5d6da3da5",
+    "url": "/wangcai-website/index.html"
+  },
+  {
+    "revision": "63695cc81c51db69598f",
+    "url": "/wangcai-website/js/app.5af038b3.js"
+  },
+  {
+    "revision": "2afdc6441990645fce21",
+    "url": "/wangcai-website/js/chunk-vendors.ec55699c.js"
+  },
+  {
+    "revision": "ff270a0a0f67cbb5ab608fa0ac3a2f5e",
+    "url": "/wangcai-website/manifest.json"
+  },
+  {
+    "revision": "73aa2a5f4bcbb62ed68c8ad6d99fd8f3",
+    "url": "/wangcai-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wangcai-website/robots.txt"
+  }
+]);
